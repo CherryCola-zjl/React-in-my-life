8bab5eba7708c2f7c361a49a8e6d864df149c9a1
zangjinlong <zjlbj2018@gmail.com>
2020-11-24 19:31:22 +0800

    react and react-dom

--- learnreact/src/index.js
+++ learnreact/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';    // react 核心库
-import ReactDOM from 'react-dom';   //react dom渲染库
+import React from './react';    // react 核心库
+import ReactDOM from './react-dom';   //react dom渲染库
 
 // jsx 语法
 ReactDOM.render(
@@ -21,30 +21,3 @@ ReactDOM.render(
   element,
   document.getElementById('root')
 );
-// {
-//   "type": "h1",
-//   "key": null,
-//   "ref": null,
-//   "props": {
-//     "className": "title",
-//     "style": {
-//       "color": "#ddd"
-//     },
-//     "children": "Hello"
-//   },
-//   "_owner": null,
-//   "_store": {}
-// }
-/**
- * 
- * @param {*} type  元素类型 
- * @param {*} config  配置对象
- * @param {*} children  子元素
- */
-function createElement(type,config,children) {
-  let props = {...config} // 展开运算符对对象实例浅拷贝
-  if(arguments.length>3){
-    children = Array.prototype.slice.call(arguments,2);
-    
-  }
-}
