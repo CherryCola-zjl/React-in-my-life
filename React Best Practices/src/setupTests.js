53c9b8dd3d5d6bf1aa2288ffe2d1649bc0a8cb0c
z <>
2022-11-07 18:11:37 +0800

    api and proxy

--- React Best Practices/src/setupTests.js
+++ React Best Practices/src/setupTests.js
@@ -1,5 +1,10 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+const proxy = require('http-proxy-middleware')
+
+module.exports = function (app) {
+    app.use(
+      proxy.createProxyMiddleware('/api',{ 
+			target:'https://dev.com',
+			changeOrigin:true,
+		})
+    )
+  }
